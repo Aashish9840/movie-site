70fdbed66f115141856b5150f6de5780df9feafd
Aashish Shah <shahaashih@gmail.com>
2025-01-12 11:15:47 +0545

    Some changes

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import { fetchMovies } from "@/services/services";
 
 export default async function Home() {
   const data = await fetchMovies();
+  console.log(data)
   return (
     <>
       <Main movies={data?.results} />
--- services/services.js
+++ services/services.js
@@ -7,7 +7,7 @@ export const fetchMovies = async () => {
     },
   };
   const res = await fetch(
-    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc",
+    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=2&sort_by=popularity.desc",
     options
   );
 
